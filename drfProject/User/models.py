1c2af9396dd312c601deae90d142c649997b0658
ByDiegreeN0 <diegoalejandrogarcia140902@gmail.com>
2024-08-02 15:36:10 -0500

    se agrego un campo a userconfig

--- drfProject/User/models.py
+++ drfProject/User/models.py
@@ -8,7 +8,10 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     phone_number = models.IntegerField(default=0)  
-    user_config = models.OneToOneField('UserConfig', null=True, on_delete=models.CASCADE)  # Corregido on_delete
+    user_config = models.OneToOneField('UserConfig', null=True, on_delete=models.CASCADE)  
+    
+    # esto no funca y no se pq
+    # role_id = models.ForeignKey('roles', null=True, on_delete=models.CASCADE)
     username = None
     
     USERNAME_FIELD = 'email'  # Campo a usar como nombre de usuario
@@ -24,6 +27,7 @@ class User(AbstractUser):
 class UserConfig(models.Model):
     user_id = models.ForeignKey('user', on_delete=models.CASCADE)
     Two_factor_authentication = models.BooleanField(default=False)
+    Enabled_Notifications = models.BooleanField(default=True)
     
     class Meta:
         db_table = 'UserConfig' # NOMBRE DE LA TABLA EN LA BASE DE DATOS
