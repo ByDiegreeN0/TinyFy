6f09f8e496eacfaf453f5d4b96137974bb266b98
Jeisson C <jeissonluffy2006@gmail.com>
2024-09-11 17:46:28 -0500

    se cambiaron login aun faltan cosas

--- FlaskApi/src/services/Auth/AuthService.py
+++ FlaskApi/src/services/Auth/AuthService.py
@@ -1,25 +1,28 @@
-# services.py
-from flask import jsonify, request
+from flask import Flask, request, jsonify
 from flask_jwt_extended import create_access_token
-from models.User import User
-from app import app, db  # Importa la instancia de Flask y db desde app.py
-
+import bcrypt
+from models.User import User, db
+from app import app
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Obtener datos del cuerpo de la solicitud
-    data = request.get_json()
+    data = request.json
     request_email = data.get('email')
     request_password = data.get('password')
-    
-    # Buscar el usuario por correo electrónico
+
+    print(f"Request Email: {request_email}")
+    print(f"Request Password: {request_password}")
+
     user = User.query.filter_by(email=request_email).first()
-    
-    # Verificar si el usuario existe y la contraseña es correcta
-    if user and user.password == request_password:
-        # Crear un token de acceso
-        access_token = create_access_token(identity=user.email)
-        return jsonify(access_token=access_token)
-    
-    # Responder con un error si la autenticación falla
-    return jsonify({"msg": "Bad username or password"}), 401
+    if user:
+        print(f"User Found: {user.username}")
+        print(f"Stored Password: {user.password}")
+
+        if bcrypt.checkpw(request_password.encode('utf-8'), user.password):
+            access_token = create_access_token(identity=user.email)
+            return jsonify(access_token=access_token)
+        else:
+            return jsonify({"msg": "Bad password"}), 401
+    else:
+        return jsonify({"msg": "Bad email"}), 401
+
