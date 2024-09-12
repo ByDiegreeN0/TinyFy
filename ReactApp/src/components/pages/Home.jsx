9fd6ea80e453318e3a415dcc5be7303556e45fe9
wvanessalomo2006@gmail.com <WenVanessa>
2024-09-12 08:06:20 -0500

    se hicieron cositas

--- ReactApp/src/components/pages/Home.jsx
+++ ReactApp/src/components/pages/Home.jsx
@@ -5,6 +5,7 @@ import Footer from "../layouts/Footer";
 import "../styles/stylesUtils/shapedividers_com-3875.css";
 import "../styles/stylesPages/Home.css";
 
+// HomeContent Component
 const HomeContent = ({ title, description, logoSrc }) => (
   <div className="home-content shapedividers_com-3875">
     <div className="description animationFade">
@@ -23,17 +24,19 @@ HomeContent.propTypes = {
   logoSrc: PropTypes.string.isRequired,
 };
 
+// BotonsHome Component
 const BotonsHome = () => (
   <div className="buttons">
-    <Link to="/Signin" className="button primary">
-      Sign in
+    <Link to="/signin" className="button primary">
+      Sign In
     </Link>
-    <Link to="/Signup" className="button secondary">
+    <Link to="/signup" className="button secondary">
       Sign Up
     </Link>
   </div>
 );
 
+// Features Component
 const Features = ({ title }) => (
   <div className="features shapedividers_com-1036">
     <h2 className="title-features">Features</h2>
@@ -42,10 +45,10 @@ const Features = ({ title }) => (
         <div className="card-content">
           <h3>Quick and easy URL shortening</h3>
           <p className="paragraph-features animationFade">
-            {title} allows you to shorten URLs quickly and easily. With a
-            intuitive interface, any user can transform links
-            long URLs in short and manageable in seconds, perfect for
-            Share on social networks and emails.
+            {title} allows you to shorten URLs quickly and easily. With an
+            intuitive interface, any user can transform long URLs into short
+            and manageable ones in seconds, perfect for sharing on social
+            networks and emails.
           </p>
         </div>
       </div>
@@ -54,10 +57,9 @@ const Features = ({ title }) => (
           <h3>Detailed link performance statistics</h3>
           <p className="paragraph-features animationFade">
             {title} offers comprehensive analytical tools that show the
-            number of clicks, the geographical location of visitors,
-            devices used and traffic sources. These statistics
-            Detailed information helps you understand link performance and
-            optimize marketing strategies.
+            number of clicks, the geographical location of visitors, devices
+            used, and traffic sources. These detailed statistics help you
+            understand link performance and optimize marketing strategies.
           </p>
         </div>
       </div>
@@ -65,22 +67,27 @@ const Features = ({ title }) => (
         <div className="card-content">
           <h3>Data security and privacy</h3>
           <p className="paragraph-features animationFade">
-            {title} prioritize the security and privacy of your data with measures
-            advanced encryption and compliance
-            international. We ensure that all personal information is
-            handle it safely and responsibly, protecting it against access
-            unauthorized.
+            {title} prioritizes the security and privacy of your data with
+            advanced encryption measures and international compliance. We
+            ensure that all personal information is handled safely and
+            responsibly, protecting it against unauthorized access.
           </p>
         </div>
       </div>
     </div>
   </div>
+
 );
 
+Features.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// Home Component
 const Home = () => {
   const title = "TinyFy";
   const description =
-    "TinyFy is an innovative URL shortener that strategically integrates banner ads. It allows users to generate custom short links that display relevant ads before redirecting to the final destination. Advertising revenue is shared with users, encouraging their use. With a focus on security, privacy and optimal performance, TinyFy facilitates efficient link management on digital platforms.";
+    "TinyFy is an innovative URL shortener that strategically integrates banner ads. It allows users to generate custom short links that display relevant ads before redirecting to the final destination. Advertising revenue is shared with users, encouraging their use. With a focus on security, privacy, and optimal performance, TinyFy facilitates efficient link management on digital platforms.";
 
   return (
     <div className="home-container">
