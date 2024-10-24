2fc04b259997ea1a95db07b2a70f0d038fefa992
Diego <136214429+ByDiegreeN0@users.noreply.github.com>
2024-10-24 17:18:57 -0500

    se crearon 2 archivos para hacer peticiones al api de adsterra y paypal respectivamente

---
+++ FlaskApi/src/API/AdsterraAPIRequest.py
@@ -0,0 +1,5 @@
+# archivo generado para colocar los endpoints y funciones necesarias para el sistema
+# de anuncio con adsterra.
+
+# de los endpoints se tienen que sacar funciones para calcular CPM, estadisticas para
+# las carcateristicas de dashboard y demas
