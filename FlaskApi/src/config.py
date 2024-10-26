f7859dc5b382cc455f9a32b9037237af075dc610
Diego <136214429+ByDiegreeN0@users.noreply.github.com>
2024-10-26 08:05:20 -0500

    se hizo el auth de paypal, se crearon todos los archivos de eventos y PaypalApiRequest, pero por ahora estos 2 ultimos no funcionan

--- FlaskApi/src/config.py
+++ FlaskApi/src/config.py
@@ -12,6 +12,12 @@ class DevelopmentConfig:
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
     MYSQL_DB = os.getenv('MYSQL_DB')
     
+    # API Keys
+    ADSTERRA_API_KEY= os.getenv('ADSTERRA_API_KEY')
+    PAYPAL_CLIENT_ID= os.getenv('PAYPAL_CLIENT_ID')
+    PAYPAL_SECRET_KEY= os.getenv('PAYPAL_SECRET_KEY')
+    PAYPAL_SANDBOX_EMAIL= os.getenv('PAYPAL_SANDBOX_EMAIL') # email de prueba de paypal, para testear pagos
+    
     # Construir la URI de la base de datos para SQLAlchemy
     SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False# Desactivar rastreo de modificaciones
