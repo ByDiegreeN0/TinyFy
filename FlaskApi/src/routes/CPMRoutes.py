3311006abd5edbf76b651b4bdd5025f9448634fa
bumbleGOD <juanjosetm.22@gmail.com>
2024-10-27 11:27:42 -0500

    función para sacar el cpm del último año hecha y cambio en el modelo del cpm para obtener los decimales del cpm

--- FlaskApi/src/routes/CPMRoutes.py
+++ FlaskApi/src/routes/CPMRoutes.py
@@ -7,12 +7,39 @@ from app import app
 from models.User import db
 from models.CPMModel import CPM
 
-from services.CPM.CpmService import obtener_cpm
+from services.CPM.CpmService import obtener_cpm, obtener_cpm_ultimo_año
 
+# Create Creación de otra ruta para crear el cpm, esta ruta es solo para mostrar a los profesores 
+# ya que trae el cpm del ultimo año o también sirve para pruebas pero no en producción REPITO
+# no en producción
+@cross_origin # implementa CORS
+@app.route('/cpm/last-year', methods=['post'])
+@jwt_required() # con este metodo se protege la ruta
+
+def create_cpm_last_year():
+    # aca se obtiene el cpm diario (por favor cambiar el api key a una variable de entorno en .env)
+    cpm_data, status_code = obtener_cpm_ultimo_año('dc5ecf146f8450b3da4082d0109595a3')
+    
+    # Esto es para descomponer el cpm ya que devuelve inicialmente un diccionario de la funcion
+    # Y pues no se pueden meter diccionarios en la db
+    if status_code == 200:
+        cpm_value = cpm_data['cpm_total']
+        
+        # Insertar en la base de datos y a dormir
+        new_cpm = CPM(cpm=cpm_value)
+        
+        db.session.add(new_cpm)
+        db.session.commit()  
+        return jsonify({'message': 'CPM created successfully'}), 201
+    else:
+        # Esto es por si ocurre un error y ademas le agregué un rollback por si el error es de la db
+        db.session.rollback()
+        return jsonify({"error": 'An error occurred'}), 500
+    
 # Create
 @cross_origin # implementa CORS
 @app.route('/cpm', methods=['POST'])
-# @jwt_required() # con este metodo se protege la ruta
+@jwt_required() # con este metodo se protege la ruta
 
 def create_cpm():
     # aca se obtiene el cpm diario (por favor cambiar el api key a una variable de entorno en .env)
@@ -39,7 +66,7 @@ def create_cpm():
 
 @cross_origin # implementa CORS
 @app.route('/cpm', methods=['GET'])
-# @jwt_required() # con este metodo se protege la ruta
+@jwt_required() # con este metodo se protege la ruta
 
 def get_cpm():
     cpms = CPM.query.all()
